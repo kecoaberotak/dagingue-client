67576f56c5d07519a9c596d6d62382dc0bdcdc09
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-04-18 15:32:09 +0700

    Add: HeroSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import TestCard from "@/components/TestCard";
+export const metadata = {
+  title: "Landing Page - Daging & Bumbu",
+  description: "Daging dan Bumbu siap masak dengan kualitas terbaik. Tersedia produk segar, higienis, dan praktis.",
+};
 
-export default function Home() {
+import HeroSection from "@/components/sections/HeroSection";
+import { getContentData } from "@/services/home.service";
+import getValueByKey from "@/utils/getValueByKey";
+
+export default async function Home() {
+  const data = await getContentData();
+
+  const heroImage = getValueByKey(data, "hero_image");
+  const logoImage = getValueByKey(data, "logo_image");
+
   return (
     <main className="min-h-screen py-10 px-4">
-      <TestCard />
+      <HeroSection hero_img={heroImage} logo_img={logoImage} />
     </main>
   );
 }
