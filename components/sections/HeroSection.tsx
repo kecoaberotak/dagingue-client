d630bdd93dacc315c6b82968b68c665a1a28ad87
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-04-18 15:40:26 +0700

    Use: component Next Image

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -3,12 +3,14 @@ type HeroProps = {
   logo_img: string;
 };
 
+import Image from "next/image";
+
 export default function HeroSection({ hero_img, logo_img }: HeroProps) {
   return (
     <section className="relative w-full h-screen flex items-center justify-center overflow-hidden">
-      <img src={hero_img} alt="Hero Image" className="absolute inset-0 w-full h-full object-cover z-0" />
+      <Image src={hero_img} alt="Hero Image" className="absolute inset-0 w-full h-full object-cover z-0" width={500} height={500} />
       <div className="relative z-10 text-center">
-        <img src={logo_img} alt="Logo" className="w-32 h-32 mx-auto mb-4" />
+        <Image src={logo_img} alt="Logo" className="w-32 h-32 mx-auto mb-4" width={700} height={700} />
         <h1 className="text-3xl md:text-5xl font-bold text-white">Selamat Datang</h1>
         <p className="text-white mt-2">Daging & Bumbu Siap Masak Terbaik</p>
       </div>
