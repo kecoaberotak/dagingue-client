dc235090a9e08c9593ddf60e9aba1d1189b5a53b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-07 20:27:11 +0700

    Create: navbar for hero section

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -11,6 +11,17 @@ export default function HeroSection({ hero_img, logo_img }: HeroProps) {
   return (
     <section className="w-full h-[379px] bg-no-repeat bg-cover bg-center mb-[50px] grid justify-center" style={{ backgroundImage: `url(${hero_img})` }} id="home">
       <Logo logo_img={logo_img} />
+      <ul className="navbar-hero">
+        <li>
+          <a href="#about">Tentang Kami</a>
+        </li>
+        <li>
+          <a href="#product">Produk</a>
+        </li>
+        <li>
+          <a href="#contact">Kontak</a>
+        </li>
+      </ul>
     </section>
   );
 }
