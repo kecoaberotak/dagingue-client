3c2ade7496fe32b5a45138a6da6fdb98350dc67b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-07 14:21:28 +0700

    Add: classname for testing custom color variabel

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -10,10 +10,8 @@ export default function HeroSection({ hero_img, logo_img }: HeroProps) {
     <section className="w-full h-[379px] bg-no-repeat bg-cover bg-center mb-[50px] grid justify-center" style={{ backgroundImage: `url(${hero_img})` }}>
       <div className="relative z-10 text-center">
         <Image src={logo_img} alt="Logo" className="w-32 h-32 mx-auto mb-4" width={700} height={700} />
-        <h1 className="text-3xl md:text-5xl font-bold text-white">Selamat Datang</h1>
-        <p className="text-white mt-2">Daging & Bumbu Siap Masak Terbaik</p>
+        <h1 className="text-3xl md:text-5xl font-bold text-[var(--merah)]">Selamat Datang</h1>
       </div>
-      <div className="absolute inset-0 bg-black/40 z-[1]" />
     </section>
   );
 }
