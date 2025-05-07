485e138a2a3e0841acb178da9701331ad30d276b
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-07 21:00:12 +0700

    Add: component Socmed

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,11 +1,15 @@
 type HeroProps = {
   hero_img: string;
   logo_img: string;
+  social_whatsapp: string;
+  social_shopee: string;
+  social_instagram: string;
 };
 
 import Logo from "../atoms/Logo";
+import Socmed from "../atoms/Socmed";
 
-export default function HeroSection({ hero_img, logo_img }: HeroProps) {
+export default function HeroSection({ hero_img, logo_img, social_instagram, social_shopee, social_whatsapp }: HeroProps) {
   console.log(logo_img);
 
   return (
@@ -22,6 +26,7 @@ export default function HeroSection({ hero_img, logo_img }: HeroProps) {
           <a href="#contact">Kontak</a>
         </li>
       </ul>
+      <Socmed social_instagram={social_instagram} social_shopee={social_shopee} social_whatsapp={social_whatsapp} />
     </section>
   );
 }
