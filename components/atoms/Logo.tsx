31605f3022ca856f62364bb6ec5990bbe058fdf5
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-08 12:02:37 +0700

    Edit: parameter name from logo_img to logo_image

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Logo({ logo_img }: { logo_img: string }) {
+export default function Logo({ logo_image }: { logo_image: string }) {
   return (
     <div className="w-full mt-[50px] grid justify-center">
-      <Image src={logo_img} alt="Logo" title="Logo Dagingue" width={283} height={233} />
+      <Image src={logo_image} alt="Logo" title="Logo Dagingue" width={283} height={233} />
     </div>
   );
 }
