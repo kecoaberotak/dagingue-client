e37fb1c4b2368db1d647da7576d0488f03346f8c
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-08 12:03:41 +0700

    Create: HomeContext for store data

---
+++ contexts/HomeContext.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { createContext, useContext, ReactNode } from "react";
+
+// Menggunakan tipe HeroProps langsung untuk context
+export type HomeContextType = {
+  hero_image: string;
+  logo_image: string;
+  social_shopee: string;
+  social_instagram: string;
+  social_whatsapp: string;
+};
+
+// Tipe context adalah Partial<HomeContextType> agar data bisa optional
+const HomeContext = createContext<Partial<HomeContextType> | undefined>(undefined);
+
+export const HomeProvider = ({ children, value }: { children: ReactNode; value: Partial<HomeContextType> }) => {
+  return <HomeContext.Provider value={value}>{children}</HomeContext.Provider>;
+};
+
+export const useHomeContext = () => {
+  const context = useContext(HomeContext);
+  if (!context) {
+    throw new Error("useHomeContext must be used within HomeProvider");
+  }
+  return context;
+};
