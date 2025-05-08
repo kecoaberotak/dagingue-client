6e85de8d70c52b3c3c9fb2f439eb19e6ae1e8ee9
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-08 14:30:58 +0700

    Add: UI for AboutSection

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useHomeContext } from "@/contexts/HomeContext";
+import Image from "next/image";
 
 export default function AboutSection() {
   const { about_us = "", img_1 = "", img_2 = "" } = useHomeContext();
@@ -8,10 +9,10 @@ export default function AboutSection() {
   return (
     <section id="about" className="about">
       <div className="image-about">
-        <img src={img_1} alt="Tentang Dagingue" title="Tentang Dagingue" className="foto-1" />
-        <img src={img_2} alt="Tentang Dagingue" title="Tentang Dagingue" className="foto-2" />
+        <Image src={img_1} alt="Tentang Dagingue" title="Tentang Dagingue" className="foto-1" width={1080} height={1080} />
+        <Image src={img_2} alt="Tentang Dagingue" title="Tentang Dagingue" className="foto-2" width={1080} height={1080} />
       </div>
-      <article className="article_about">
+      <article className="article-about">
         <h1>Tentang Dagingue Bogor</h1>
         <div dangerouslySetInnerHTML={{ __html: about_us }} data-testid="article-content"></div>
       </article>
