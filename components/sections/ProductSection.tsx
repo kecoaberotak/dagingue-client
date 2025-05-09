d60b91fee5daa2ff9d1b61628398786f3f6949dd
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-09 12:49:17 +0700

    Add: Banner for product section

--- components/sections/ProductSection.tsx
+++ components/sections/ProductSection.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+// import { PotongProvider } from "@/contexts/PotongContext";
+// import { getPotongData } from "@/services/product.service";
+import { useHomeContext } from "@/contexts/HomeContext";
+
 export default function ProductSection() {
+  const { banner_product } = useHomeContext();
   return (
-    <section className="product" id="product">
+    <section className="product" id="product" style={{ backgroundImage: `url(${banner_product})` }}>
       <div className="product-potong">
         <section className="title-section potong">
           <h3>Produk</h3>
