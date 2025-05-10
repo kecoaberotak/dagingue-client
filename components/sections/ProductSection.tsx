ed425deeda8784832afc1aa3547c5b112014a0a9
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-10 15:39:57 +0700

    Add: UI for card

--- components/sections/ProductSection.tsx
+++ components/sections/ProductSection.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { useHomeContext } from "@/contexts/HomeContext";
+import { usePotongContext } from "@/contexts/PotongContext";
 import Title from "../atoms/Title";
+import Card from "../molecules/Card";
 
 export default function ProductSection() {
   const { banner_product } = useHomeContext();
+  const data = usePotongContext();
+
+  let classname = "";
+  if (data) {
+    if (data.length > 2) {
+      classname = "cards-product-3";
+    } else if (data.length < 2) {
+      classname = "cards-product-1";
+    } else classname = "cards-product-2";
+  }
 
   return (
     <section className="product" id="product" style={{ backgroundImage: `url(${banner_product})` }}>
       <Title classname="title-section potong" title="Produk" subTitle="Jenis Potongan" />
+      <section className={classname}>
+        {data.map((potong) => {
+          return <Card key={potong.id} nama={potong.nama} gambar={potong.gambar} berat={potong.berat} />;
+        })}
+      </section>
     </section>
   );
 }
