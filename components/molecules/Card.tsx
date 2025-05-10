bc012b2c2c39665882646f5b71adf3dbb78afa12
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-10 17:14:45 +0700

    Edit: use Image for card image

--- components/molecules/Card.tsx
+++ components/molecules/Card.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
+
 const Card = ({ nama, gambar, berat }: { nama: string; gambar: string; berat: string }) => {
   return (
     <div className="card" data-testid="card-potong">
       <section className="card-product">
-        <img className="potong-img" src={gambar} alt={nama} title={nama} />
+        <Image className="potong-img" src={gambar} alt={nama} title={nama} width={1080} height={1080} />
       </section>
       <section className="card-product-info">
         <p>{nama}</p>
