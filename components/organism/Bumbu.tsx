23835cda54d03632780d7098317bc28fa3994976
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-12 14:49:08 +0700

    Add: SelectedBumbuProvider

--- components/organism/Bumbu.tsx
+++ components/organism/Bumbu.tsx
@@ -1,14 +1,17 @@
 import Title from "../atoms/Title";
 import ListBumbu from "../molecules/ListBumbu";
 import InfoBumbu from "../molecules/InfoBumbu";
+import { SelectedBumbuProvider } from "@/contexts/SelectedBumbuContext";
 
 export default function Bumbu() {
   return (
     <section className="product-bumbu">
       <Title classname="title-section bumbu" title="Produk" subTitle="Varian Bumbu" />
       <div className="gallery">
-        <InfoBumbu />
-        <ListBumbu />
+        <SelectedBumbuProvider>
+          <InfoBumbu />
+          <ListBumbu />
+        </SelectedBumbuProvider>
       </div>
     </section>
   );
