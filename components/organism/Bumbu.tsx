c9af90d482065ecba2e40fcb9d843fbba5d6872e
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-12 11:03:32 +0700

    Create: component Bumbu

---
+++ components/organism/Bumbu.tsx
@@ -0,0 +1,12 @@
+import Title from "../atoms/Title";
+
+export default function Bumbu() {
+  return (
+    <section className="product-bumbu">
+      <Title classname="title-section bumbu" title="Produk" subTitle="Varian Bumbu" />
+      <div className="gallery-bumbu">
+        <></>
+      </div>
+    </section>
+  );
+}
