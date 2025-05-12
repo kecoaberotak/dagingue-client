9634702bd33b839fc4cc594425d0e5c28784f30c
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-12 13:12:20 +0700

    Create: UI for gallery bumbu

--- components/organism/Bumbu.tsx
+++ components/organism/Bumbu.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import { useBumbuContext } from "@/contexts/BumbuContext";
 import Title from "../atoms/Title";
+import Image from "next/image";
 
 export default function Bumbu() {
+  const data = useBumbuContext();
+
   return (
     <section className="product-bumbu">
       <Title classname="title-section bumbu" title="Produk" subTitle="Varian Bumbu" />
-      <div className="gallery-bumbu">
-        <></>
+      <div className="gallery">
+        <section className="gallery-info">
+          <div className="gallery-info-image">
+            <Image src={data[0].gambar} alt={data[0].nama} title={data[0].nama} width={1080} height={1080} className="default" />
+          </div>
+          <div className="gallery-info-bumbu">
+            <h2>{data[0].nama}</h2>
+            <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: data[0].deskripsi }} />
+          </div>
+        </section>
+        <section className="gallery-items">
+          {data.map((bumbu) => {
+            return (
+              <div className="gallery-item-image" key={bumbu.id}>
+                <Image width={1080} height={1080} src={bumbu.gambar} alt={bumbu.nama} title={bumbu.nama} />
+              </div>
+            );
+          })}
+        </section>
       </div>
     </section>
   );
