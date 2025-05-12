df4e2e0afb4c9f0a9aa3cc707bb5a16bb0ef0675
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-12 16:26:33 +0700

    Create: func handle selected image

--- components/molecules/ListBumbu.tsx
+++ components/molecules/ListBumbu.tsx
@@ -2,16 +2,37 @@
 
 import Image from "next/image";
 import { useBumbuContext } from "@/contexts/BumbuContext";
+import { useBumbuSelected } from "@/contexts/SelectedBumbuContext";
 
 const ListBumbu = () => {
   const data = useBumbuContext();
+  const { setSelectedBumbu } = useBumbuSelected();
+
+  const handleSelectedImage = (e: React.MouseEvent<HTMLImageElement>) => {
+    const target = e.target as HTMLImageElement;
+
+    setSelectedBumbu({
+      nama: target.getAttribute("data-name") ?? "",
+      gambar: target.getAttribute("data-image") ?? "",
+      deskripsi: target.getAttribute("data-desc") ?? "",
+    });
+
+    const selectedImage = document.querySelectorAll(".cover");
+    selectedImage.forEach((image) => {
+      if (image.classList.contains("selected")) {
+        image.classList.remove("selected");
+      }
+    });
+    target.classList.add("selected");
+  };
 
   return (
-    <section className="list-bumbu">
+    <section className="list-bumbu" onClick={handleSelectedImage}>
       {data.map((bumbu) => {
         return (
           <div className="list-bumbu-image" key={bumbu.id}>
             <Image width={1080} height={1080} src={bumbu.gambar} alt={bumbu.nama} title={bumbu.nama} />
+            <div data-name={bumbu.nama} data-desc={bumbu.deskripsi} data-image={bumbu.gambar} className="cover" />
           </div>
         );
       })}
