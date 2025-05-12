b2be71ff623b63dad20b67171aba21c8b423b259
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-12 16:37:05 +0700

    Add: SelectedBumbuContext for InfoBumbu

--- components/molecules/InfoBumbu.tsx
+++ components/molecules/InfoBumbu.tsx
@@ -2,18 +2,20 @@
 
 import Image from "next/image";
 import { useBumbuContext } from "@/contexts/BumbuContext";
+import { useBumbuSelected } from "@/contexts/SelectedBumbuContext";
 
 const InfoBumbu = () => {
   const data = useBumbuContext();
+  const { selectedBumbu } = useBumbuSelected();
 
   return (
     <section className="info-bumbu">
       <div className="selected-bumbu-image">
-        <Image src={data[0].gambar} alt={data[0].nama} title={data[0].nama} width={1080} height={1080} className="default" />
+        <Image src={selectedBumbu ? selectedBumbu.gambar : data[0].gambar} alt={selectedBumbu ? selectedBumbu.nama : data[0].nama} title={selectedBumbu ? selectedBumbu.nama : data[0].nama} width={1080} height={1080} className="default" />
       </div>
       <div className="selected-bumbu-desc">
-        <h2>{data[0].nama}</h2>
-        <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: data[0].deskripsi }} />
+        <h2>{selectedBumbu ? selectedBumbu.nama : data[0].nama}</h2>
+        <div className="w-[300px] h-[120px] sm:w-[376px] sm:h-[130px]" dangerouslySetInnerHTML={{ __html: selectedBumbu ? selectedBumbu.deskripsi : data[0].deskripsi }} />
       </div>
     </section>
   );
