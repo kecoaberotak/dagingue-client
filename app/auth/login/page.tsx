9c621548729c91e4598da06da6f59e720ab99f42
Dennis Fernandes <dennisfernandes0212@gmail.com>
2025-05-15 15:15:18 +0700

    Edit: typo

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,7 +2,7 @@ export default function LoginPage() {
   return (
     <form className="form-login">
       <p>Login</p>
-      <input type="text" placeholder="Uusername" />
+      <input type="text" placeholder="Username" />
       <input type="password" placeholder="password" />
       <button className="button login-button">Login</button>
     </form>
